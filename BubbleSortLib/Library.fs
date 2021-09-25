745519bb73c8a46efc30c79cc609dc3b3b55b3a6
svetlanayo <svetlanya05@mail.ru>
2021-09-25 17:56:05 +0300

    Init commit

---
+++ BubbleSortLib/Library.fs
@@ -0,0 +1,25 @@
+﻿namespace BubbleSortLib
+
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+
+module Sorter =
+
+
+    let swap i j (arr : 'a []) =
+        let tmp = arr.[i]
+        arr.[i] <- arr.[j]
+        arr.[j] <- tmp
+
+    let sort arr =
+        let rec loop (arr : 'a []) =
+            let mutable swaps = 0
+            for i = 0 to arr.Length - 2 do
+                if arr.[i] > arr.[i+1] then
+                    swap i (i+1) arr
+                    swaps <- swaps + 1
+
+            if swaps > 0 then loop arr else arr
+
+        loop arr
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BubbleSortLib;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 7, 52, 34, 4, 15, 60 };
+            BubbleSortLib.Sorter.sort(array);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
